edea10ece8260eb453f8fcc62022ab351c3d301b
Igor Follador <igorledf@hotmail.com>
2022-08-31 20:53:32 -0300

    Create home page and page telaEmpresa

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,71 @@
+import 'package:atm_consultoria/TelaEmpresa.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  void _abrirEmpresa() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => TelaEmpresa("Elilus")));
+  }
+
+  void _abrirServico() {}
+  void _abrirCliente() {}
+  void _abrirContato() {}
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("ATM Consultoria"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/logo.png"),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: _abrirEmpresa,
+                      child: Image.asset("images/menu_empresa.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _abrirServico,
+                      child: Image.asset("images/menu_servico.png"),
+                    )
+                  ]),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: _abrirCliente,
+                      child: Image.asset("images/menu_cliente.png"),
+                    ),
+                    GestureDetector(
+                      onTap: _abrirContato,
+                      child: Image.asset("images/menu_contato.png"),
+                    )
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
